201ce0ccf2b8465862112f22b32ca18abb215a18
eetu <eetuvalve70@hotmail.com>
2019-08-20 13:48:59 +0300

    add translations and scaling

---
+++ script.js
@@ -0,0 +1,52 @@
+var finnishTranslations = {
+  quote: "Etsitkö Full-Stack kehittäjää projektiin?",
+  text2: "(älä epäröi olla yhteydessä)",
+  manyMore: "...ja monta muuta",
+  email: "Sähköposti:",
+  businessId: "Y-tunnus",
+  billingAddress: "Laskutusosoite"
+}
+
+var englishTranslations = {
+  quote: "Looking for a Full-Stack developer for your project?",
+  text2: "(do not hesitate to contact)",
+  manyMore: "...and many more",
+  email: "Email:",
+  businessId: "Business ID:",
+  billingAddress: "Billing address:"
+}
+
+window.onload = function () {
+
+  var activeLang = 'en';
+  setLang(englishTranslations)
+  setActiveColor()
+
+  document.getElementById('en').addEventListener('click', function () {
+    activeLang = 'en'
+    setActiveColor('fi')
+    setLang(englishTranslations)
+  });
+
+  document.getElementById('fi').addEventListener('click', function () {
+    activeLang = 'fi'
+    setActiveColor('en')
+    setLang(finnishTranslations)
+  });
+
+  function setLang(selectedLang) {
+    Object.keys(selectedLang).map(function (value, key) {
+      document.getElementById(value).textContent = selectedLang[value]
+    })
+  }
+
+  function setActiveColor(removeActive) {
+    if (removeActive) {
+      document.getElementById(removeActive).style.color = "black"
+    }
+    document.getElementById(activeLang).style.color = "#fc5367"
+  }
+
+
+}
+
